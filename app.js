5b1b80e8866d9ae08322e9b2099b380757241cc6
soumyaa77 <109790494+soumyaa77@users.noreply.github.com>
2023-07-28 20:29:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -283,6 +283,6 @@ app.post("/update", (req, res) => {
   );
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running...")
+})
